281ce69cc5cf4b15b9234ecb39a5a4a32118a4c7
Alex Fox <alextsfox@gmail.com>
2023-01-23 11:20:01 -0700

    Update tilt_corrections.py

--- tilt_corrections.py
+++ tilt_corrections.py
@@ -3,7 +3,14 @@
 import numpy as np
 from scipy import optimize
 
-__all__ = ['get_double_rotation_angles', 'fourier_func', 'dfourier_func_dtheta', 'get_continuous_planar_fit_angles', 'double_rotation_fit_from_angles', 'double_rotation_fit_from_uvw', 'continuous_planar_fit_from_angles', 'continuous_planar_fit_from_uvw']
+__all__ = ['get_double_rotation_angles', 
+           'fourier_func', 
+           'dfourier_func_dtheta', 
+           'get_continuous_planar_fit_angles', 
+           'double_rotation_fit_from_angles', 
+           'double_rotation_fit_from_uvw', 
+           'continuous_planar_fit_from_angles', 
+           'continuous_planar_fit_from_uvw']
 
 def get_double_rotation_angles(U, V, W):
     '''
